94a285e3469f0507e75be91d33c400946963c078
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-10-28 19:24:08 +0200

    Improved styling

--- src/components/article/article.tsx
+++ src/components/article/article.tsx
@@ -3,12 +3,14 @@ import { addArticle } from "../cart/use-cart";
 
 export default function Article(props: { article: Article }) {
   return (
-    <div className="border border-blue-500 rounded bg-blue-900 p-4">
-      <div>{props.article.name}</div>
-      <div>{props.article.description}</div>
-      <div>{props.article.price}</div>
+    <div className="flex flex-col justify-between gap-4 rounded border border-blue-200 bg-blue-50 p-4 md:flex-row md:items-end">
+      <div>
+        <div className="font-semibold">{props.article.name}</div>
+        <div>{props.article.description}</div>
+        <div>CHF {props.article.price}</div>
+      </div>
       <button
-        className="rounded bg-white text-blue-900 px-6 py-1"
+        className="rounded bg-blue-900 px-6 py-1 text-white"
         onClick={() => addArticle(props.article)}
       >
         Add to cart
