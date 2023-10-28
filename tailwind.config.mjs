57ef48cffc93052de1cefd7378b21879d888a7f3
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-10-28 19:19:14 +0200

    Set prettier as formatter

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "2rem",
+        xl: "2rem",
+        "2xl": "2rem",
+      },
+    },
+    extend: {
+      screens: {
+        lg: "1024px",
+        xl: "1024px",
+        "2xl": "1024px",
+      },
+    },
+  },
+  plugins: [],
+};
