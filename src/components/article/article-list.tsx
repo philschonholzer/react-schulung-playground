8051f3e89aed74d048fe8fd2ffed6f67b01c3bae
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-10-29 09:35:18 +0100

    Fetch articles in react at build time

--- src/components/article/article-list.tsx
+++ src/components/article/article-list.tsx
@@ -1,11 +1,14 @@
 import { makeArticle } from "../../domain/article/example-data";
 import type { Article as ArticleType } from "../../domain/article/types";
 import Article from "./article";
+const articles = (await fetch("http://localhost:3004/articles").then(
+  (response) => response.json(),
+)) as ArticleType[];
 
-export default function ArticleList(props: { articles: ArticleType[] }) {
+export default function ArticleList() {
   return (
     <ul className="space-y-2 ">
-      {props.articles.map((article) => (
+      {articles.map((article) => (
         <li className="" key={article.id}>
           <Article article={article} />
         </li>
